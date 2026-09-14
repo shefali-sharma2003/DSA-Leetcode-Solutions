624c1b6bbba99f894dc030ffa093cb0b7ff52738
shefali-sharma2003 <shefalisharma4636@gmail.com>
2026-09-14 13:27:49 +0530

    Time: 68 ms (5.14%), Space: 46.4 MB (41.67%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+        String s1=new String(ch1);
+        String s2=new String(ch2);
+
+        if(s1.equals(s2)) return true;
+        else return false;
+    }
+}
